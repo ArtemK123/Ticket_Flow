b9561780217fffa814ffc209e393d67c52dd7ffb
unknown <darkmode1012@gmail.com>
2020-05-26 21:47:47 +0300

    Added profile-service

--- config_server/src/main/java/com/ticketflow/config_server/ConfigServerApplication.java
+++ config_server/src/main/java/com/ticketflow/config_server/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.config_server;
+package com.ticketflow.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
