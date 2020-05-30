35ccd8616c275ddfde832674a5c98b0f43a944f5
ArtemK123 <darkmode1012@gmail.com>
2020-05-30 13:11:50 +0300

    Fixed warning

--- identity_service/src/main/java/com/ticketflow/identity_service/api/UsersApiController.java
+++ identity_service/src/main/java/com/ticketflow/identity_service/api/UsersApiController.java
@@ -10,7 +10,6 @@ import com.ticketflow.identity_service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
