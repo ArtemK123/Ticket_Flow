5936d254273b34d4ae4b885f150e4f6b1bca6bec
ArtemK123 <darkmode1012@gmail.com>
2020-06-01 15:49:47 +0300

    Added backend servcie factory

--- frontend/ticketflow/src/components/login_page/LoginPage.js
+++ frontend/ticketflow/src/components/login_page/LoginPage.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import createBackendService from "../backend_service/createBackendService";
 
 function LoginPage() {
+    const backendService = createBackendService();
     return (
         <div>
             <h3>LoginPage</h3>
+            <div>{backendService.login()}</div>
             <ul>
                 <li><Link to="/">HomePage</Link></li>
             </ul>
