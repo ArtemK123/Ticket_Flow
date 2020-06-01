6d0b812b8411154ae037a12a9feda4874736fafc
ArtemK123 <darkmode1012@gmail.com>
2020-06-01 14:27:17 +0300

    Added simple redirect in register page

--- frontend/ticketflow/src/components/register_page/RegisterPage.js
+++ frontend/ticketflow/src/components/register_page/RegisterPage.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 function RegisterPage() {
+
+    const history = useHistory();
     return (
-        <div>RegisterPage</div>
+        <div>
+            <h3>RegisterPage</h3>
+            <Button variant="contained" color="primary" onClick={() => { history.push("/"); }}>Go to Home Page</Button>
+        </div>
     );
 }
 
