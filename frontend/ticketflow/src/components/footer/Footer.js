0a7d991fab1ac41837df86f69301ac2a59818151
ArtemK123 <darkmode1012@gmail.com>
2020-06-03 23:09:35 +0300

    Changed the footer behaviour

--- frontend/ticketflow/src/components/footer/Footer.js
+++ frontend/ticketflow/src/components/footer/Footer.js
@@ -5,35 +5,12 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles((theme) => ({
-    text: {
-        padding: theme.spacing(2, 2, 0),
-    },
-    paper: {
-        paddingBottom: 50,
-    },
-    list: {
-        marginBottom: theme.spacing(2),
-    },
-    subheader: {
-        backgroundColor: theme.palette.background.paper,
-    },
+const useStyles = makeStyles(() => ({
     appBar: {
         top: "auto",
         bottom: 0,
         backgroundColor: "white",
         color: "grey" 
-    },
-    grow: {
-        flexGrow: 1,
-    },
-    fabButton: {
-        position: "absolute",
-        zIndex: 1,
-        top: -30,
-        left: 0,
-        right: 0,
-        margin: "0 auto",
     },
 }));
 
@@ -41,7 +18,7 @@ function Footer() {
     const styles = useStyles();
 
     return (
-        <AppBar position="fixed" className={styles.appBar}>
+        <AppBar position="static" className={styles.appBar}>
             <Toolbar>
                 <Grid container justify="space-between" direction="row">
                     <Grid item>
