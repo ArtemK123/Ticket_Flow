aa083620f9da96011131ad5a40ba032aedcfa5f1
ArtemK123 <darkmode1012@gmail.com>
2020-06-03 19:39:57 +0300

    Implemented footer

--- frontend/ticketflow/src/components/footer/Footer.js
+++ frontend/ticketflow/src/components/footer/Footer.js
@@ -1,8 +1,56 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+    text: {
+        padding: theme.spacing(2, 2, 0),
+    },
+    paper: {
+        paddingBottom: 50,
+    },
+    list: {
+        marginBottom: theme.spacing(2),
+    },
+    subheader: {
+        backgroundColor: theme.palette.background.paper,
+    },
+    appBar: {
+        top: "auto",
+        bottom: 0,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    fabButton: {
+        position: "absolute",
+        zIndex: 1,
+        top: -30,
+        left: 0,
+        right: 0,
+        margin: "0 auto",
+    },
+}));
 
 function Footer() {
+    const styles = useStyles();
+
     return (
-        <div>Footer</div>
+        <AppBar position="fixed" color="grey" className={styles.appBar}>
+            <Toolbar>
+                <Grid container justify="space-between" direction="row">
+                    <Grid item>
+                        <Typography>TicketFlow - All rights are reserved</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography>Contact with us: ticketflow-support@gmail.com</Typography>
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar>
     );
 }
 
