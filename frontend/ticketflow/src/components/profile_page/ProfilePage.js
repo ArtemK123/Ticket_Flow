e39378ee5a03bcdea9a96b6bb4ac39098b6ab49c
ArtemK123 <darkmode1012@gmail.com>
2020-06-04 19:33:00 +0300

    Fixed placement of inputs in profile

--- frontend/ticketflow/src/components/profile_page/ProfilePage.js
+++ frontend/ticketflow/src/components/profile_page/ProfilePage.js
@@ -2,6 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import ReadonlyTextInput from "components/common/ReadonlyTextInput";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
 
 ProfilePage.propTypes = {
     isUserLoggedIn: PropTypes.bool,
@@ -33,37 +36,49 @@ function ProfilePage(props) {
     }
 
     return (
-        <div>
-            <h3>Your profile</h3>
-            <div>
-                <ReadonlyTextInput
-                    label="Email"
-                    value={props.profileModel.userEmail}
-                    isMultiline={false}
-                />
-            </div>
-            <div>
-                <ReadonlyTextInput
-                    label="Phone number"
-                    value={props.profileModel.profile.phoneNumber}
-                    isMultiline={false}
-                />
-            </div>
-            <div>
-                <ReadonlyTextInput
-                    label="Birthday"
-                    value={props.profileModel.profile.birthday}
-                    isMultiline={false}
-                />
-            </div>
-            <div>
-                <ReadonlyTextInput
-                    label="Tickets"
-                    value={getTicketsValue(props.tickets)}
-                    isMultiline={true}
-                />
-            </div>
-        </div>
+        <Box w={1}>
+            <Grid container direction="row">
+                <Grid item xs={1}/>
+                <Grid item xs={6}>
+                    <Box m={5}>
+                        <Grid container direction="column" spacing={2}>
+                            <Grid item>
+                                <Typography variant="h4">Your profile</Typography>
+                            </Grid>
+                            <Grid item>
+                                <ReadonlyTextInput
+                                    label="Email"
+                                    value={props.profileModel.userEmail}
+                                    isMultiline={false}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <ReadonlyTextInput
+                                    label="Phone number"
+                                    value={props.profileModel.profile.phoneNumber}
+                                    isMultiline={false}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <ReadonlyTextInput
+                                    label="Birthday"
+                                    value={props.profileModel.profile.birthday}
+                                    isMultiline={false}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <ReadonlyTextInput
+                                    label="Tickets"
+                                    value={getTicketsValue(props.tickets)}
+                                    isMultiline={true}
+                                />
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Grid>
+                <Grid item xs={5}/>
+            </Grid>
+        </Box>
     );
 }
 
