bb741fe8b583f08bc0d735aab5af49f773f0d062
ArtemK123 <darkmode1012@gmail.com>
2020-07-03 08:12:00 +0300

    Launched the Hello world page

---
+++ backend/dotnet/ticketflow/ProfileService/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProfileService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [HttpGet("/")]
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
