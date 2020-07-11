0390960f9bf24e3a9449c6260039abc7123ea857
ArtemK123 <darkmode1012@gmail.com>
2020-07-11 12:31:08 +0300

    Added exception handler

---
+++ backend/dotnet/ticketflow/ProfileService/Api/Controllers/ExceptionHandlerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProfileService.Models.Exceptions;
+
+namespace ProfileService.Api.Controllers
+{
+    [Route("/error")]
+    public class ExceptionHandlerController : ControllerBase
+    {
+        public IActionResult Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var exception = context?.Error;
+
+            if (exception is NotFoundException)
+            {
+                return new NotFoundResult();
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
--- backend/dotnet/ticketflow/ProfileService/Startup.cs
+++ backend/dotnet/ticketflow/ProfileService/Startup.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using ProfileService.Domain;
 using ProfileService.Domain.Providers;
 using ProfileService.Domain.Repositories;
@@ -22,10 +21,7 @@ namespace ProfileService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseExceptionHandler("/error");
 
             app.UseRouting();
 
