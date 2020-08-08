ee06de1b5c405d7ad970c19e65ac9d6d4ba2d600
ArtemK123 <darkmode1012@gmail.com>
2020-08-08 09:24:35 +0300

    Added exception handler in Ticket service

---
+++ backend/dotnet/ticketflow/TicketFlow.TicketService/WebApi/Controllers/ExceptionHandlerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TicketFlow.TicketService.Domain.Exceptions;
+
+namespace TicketFlow.TicketService.WebApi.Controllers
+{
+    [Route("/error")]
+    public class ExceptionHandlerController : ControllerBase
+    {
+        public IActionResult Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var exception = context?.Error;
+
+            if (exception == null || exception is NotFoundException)
+            {
+                return new NotFoundResult();
+            }
+
+            if (exception is TicketAlreadyOrderedException)
+            {
+                return new BadRequestObjectResult(exception.Message);
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
