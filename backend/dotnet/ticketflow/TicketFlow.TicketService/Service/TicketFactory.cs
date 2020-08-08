ed18b3f243f072f9a71fc226d906cbfaca868a42
ArtemK123 <darkmode1012@gmail.com>
2020-08-08 10:08:13 +0300

    Added input validation in TicketFactory

--- backend/dotnet/ticketflow/TicketFlow.TicketService/Service/TicketFactory.cs
+++ backend/dotnet/ticketflow/TicketFlow.TicketService/Service/TicketFactory.cs
@@ -1,4 +1,5 @@
-﻿using TicketFlow.Common.Providers;
+﻿using System;
+using TicketFlow.Common.Providers;
 using TicketFlow.TicketService.Domain.Entities;
 using TicketFlow.TicketService.Domain.Models;
 
@@ -6,6 +7,7 @@ namespace TicketFlow.TicketService.Service
 {
     internal class TicketFactory : ITicketFactory
     {
+        private const string ShouldBePossibleExceptionMessage = "{0} should be positive";
         private readonly IRandomValueProvider randomValueProvider;
 
         public TicketFactory(IRandomValueProvider randomValueProvider)
@@ -15,8 +17,27 @@ namespace TicketFlow.TicketService.Service
 
         public ITicket CreateTicket(TicketModelWithoutId ticketModel)
         {
+            ValidateTicketModel(ticketModel);
             int ticketId = randomValueProvider.GetRandomInt(0, int.MaxValue);
             return new Ticket(ticketId, ticketModel.MovieId, ticketModel.BuyerEmail, ticketModel.Row, ticketModel.Seat, ticketModel.Price);
+        }
+
+        private static void ValidateTicketModel(TicketModelWithoutId ticketModel)
+        {
+            if (ticketModel.Row < 0)
+            {
+                throw new ArgumentException(string.Format(ShouldBePossibleExceptionMessage, "Row number"));
+            }
+
+            if (ticketModel.Seat < 0)
+            {
+                throw new ArgumentException(string.Format(ShouldBePossibleExceptionMessage, "Seat number"));
+            }
+
+            if (ticketModel.Price < 0)
+            {
+                throw new ArgumentException(string.Format(ShouldBePossibleExceptionMessage, "Price"));
+            }
         }
     }
 }
