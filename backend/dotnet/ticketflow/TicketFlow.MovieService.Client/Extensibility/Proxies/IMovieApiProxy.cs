021f4c6cac60f6d1bec10ccd962f762a174571ed
ArtemK123 <darkmode1012@gmail.com>
2020-09-04 09:50:53 +0300

    Fixed bug in MovieService.Client

--- backend/dotnet/ticketflow/TicketFlow.MovieService.Client/Extensibility/Proxies/IMovieApiProxy.cs
+++ backend/dotnet/ticketflow/TicketFlow.MovieService.Client/Extensibility/Proxies/IMovieApiProxy.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using TicketFlow.MovieService.Client.Extensibility.Entities;
 using TicketFlow.MovieService.Client.Extensibility.Models.MovieModels;
 
@@ -10,7 +9,7 @@ namespace TicketFlow.MovieService.Client.Extensibility.Proxies
     {
         public Task<IReadOnlyCollection<IMovie>> GetAllAsync();
 
-        public Task<IMovie> GetByIdAsync([FromRoute] int id);
+        public Task<IMovie> GetByIdAsync(int id);
 
         public Task<int> AddAsync(MovieCreationIdReferencedModel movieCreationIdReferencedModel);
     }
