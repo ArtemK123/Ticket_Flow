0513b5cfefa28772fddd285898c8f4d5841bf583
ArtemK123 <darkmode1012@gmail.com>
2020-09-12 08:50:33 +0300

    Added support of UrlFromConfig to all microservices

--- backend/dotnet/ticketflow/TicketFlow.TicketService/Program.cs
+++ backend/dotnet/ticketflow/TicketFlow.TicketService/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TicketFlow.Common.Providers;
 
 namespace TicketFlow.TicketService
 {
@@ -18,6 +20,17 @@ namespace TicketFlow.TicketService
                     logging.ClearProviders();
                     logging.AddConsole();
                 })
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    IConfigurationRoot appsettings =
+                        new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+
+                    IUrlFromConfigProvider urlFromConfigProvider = new UrlFromConfigProvider();
+
+                    webBuilder.UseUrls(urlFromConfigProvider.GetUrl(appsettings));
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
