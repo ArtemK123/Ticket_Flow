8e3f6193d6ba973ed8eb8805803edba7527e236f
ArtemK123 <darkmode1012@gmail.com>
2020-09-13 18:39:03 +0300

    Changed test framework to xUnit, added NSubstitute support in TicketFlow.Common.Test.csproj and wrote the first test

--- backend/dotnet/ticketflow/TicketFlow.Common/Providers/PostgresConnectionStringProvider.cs
+++ backend/dotnet/ticketflow/TicketFlow.Common/Providers/PostgresConnectionStringProvider.cs
@@ -13,10 +13,10 @@ namespace TicketFlow.Common.Providers
 
         public string GetConnectionString()
         {
-            string host = configuration["Database:Host"];
-            string databaseName = configuration["Database:Name"];
-            string user = configuration["Database:User"];
-            string password = configuration["Database:Password"];
+            string host = configuration.GetValue<string>("Database:Host");
+            string databaseName = configuration.GetValue<string>("Database:Name");
+            string user = configuration.GetValue<string>("Database:User");
+            string password = configuration.GetValue<string>("Database:Password");
 
             return $"Host={host};Username={user};Password={password};Database={databaseName}";
         }
