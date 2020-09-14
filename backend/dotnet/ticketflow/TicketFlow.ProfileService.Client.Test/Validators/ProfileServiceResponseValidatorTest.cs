84f32f3bfda68f93367ba040e271c6bbc58f5160
ArtemK123 <darkmode1012@gmail.com>
2020-09-14 21:41:45 +0300

    Added test project for each business-related project. Every test project use xUnit+NSubstitute. Structured tests, nugets and services in virtual solution folders

---
+++ backend/dotnet/ticketflow/TicketFlow.ProfileService.Client.Test/Validators/ProfileServiceResponseValidatorTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TicketFlow.Common.Exceptions;
+using TicketFlow.ProfileService.Client.Validators;
+using Xunit;
+
+namespace TicketFlow.ProfileService.Client.Test.Validators
+{
+    public class ProfileServiceResponseValidatorTest
+    {
+        private const string ExpectedExceptionMessage = "test message";
+        private const string ExpectedInternalServiceExceptionMessage = "Internal error in Profile Service";
+
+        private readonly ProfileServiceResponseValidator profileServiceResponseValidator;
+
+        public ProfileServiceResponseValidatorTest()
+        {
+            profileServiceResponseValidator = new ProfileServiceResponseValidator();
+        }
+
+        [Fact]
+        public async Task ValidateAsync_NotFound_ShouldThrowNotFoundException_Async()
+        {
+            await RunValidateAsyncExceptionTestAsync<NotFoundException>(
+                new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    Content = new StringContent(ExpectedExceptionMessage)
+                },
+                ExpectedExceptionMessage);
+        }
+
+        [Fact]
+        public async Task ValidateAsync_BadRequest_ShouldThrowNotUniqueEntityException_Async()
+        {
+            await RunValidateAsyncExceptionTestAsync<NotUniqueEntityException>(
+                new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Content = new StringContent(ExpectedExceptionMessage)
+                },
+                ExpectedExceptionMessage);
+        }
+
+        [Fact]
+        public async Task ValidateAsync_InternalServerError_ShouldThrowInternalServiceException_Async()
+        {
+            await RunValidateAsyncExceptionTestAsync<InternalServiceException>(
+                new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.InternalServerError
+                },
+                ExpectedInternalServiceExceptionMessage);
+        }
+
+        [Fact]
+        public async Task ValidateAsync_ByDefault_ShouldPass()
+        {
+            await profileServiceResponseValidator.ValidateAsync(new HttpResponseMessage { StatusCode = HttpStatusCode.Forbidden });
+        }
+
+        private async Task RunValidateAsyncExceptionTestAsync<TException>(HttpResponseMessage httpResponseMessage, string exceptionMessage)
+            where TException : Exception
+        {
+            TException exception = await Assert.ThrowsAsync<TException>(() => profileServiceResponseValidator.ValidateAsync(httpResponseMessage));
+
+            Assert.Equal(exceptionMessage, exception.Message);
+        }
+    }
+}
