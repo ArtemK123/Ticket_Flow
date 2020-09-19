58a7962496be1fb9feb1f88caeb8d8e39d0f0923
ArtemK123 <darkmode1012@gmail.com>
2020-09-19 19:59:18 +0300

    Added IdentityServiceUrlProviderTest

---
+++ backend/dotnet/ticketflow/TicketFlow.IdentityService.Client.Test/Providers/IdentityServiceUrlProviderTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using TicketFlow.IdentityService.Client.Providers;
+using Xunit;
+
+namespace TicketFlow.IdentityService.Client.Test.Providers
+{
+    public class IdentityServiceUrlProviderTest
+    {
+        private const string UrlSettingValue = "http://localhost:9001";
+
+        private readonly IReadOnlyDictionary<string, string> settings = new Dictionary<string, string>
+        {
+            { "TicketFlow:IdentityService:Url", UrlSettingValue }
+        };
+
+        private readonly IdentityServiceUrlProvider identityServiceUrlProvider;
+
+        public IdentityServiceUrlProviderTest()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(settings)
+                .Build();
+
+            identityServiceUrlProvider = new IdentityServiceUrlProvider(configuration);
+        }
+
+        [Fact]
+        public void GetUrl_ShouldGetUrlFromConfiguration()
+        {
+            var actualUrl = identityServiceUrlProvider.GetUrl();
+
+            Assert.Equal(UrlSettingValue, actualUrl);
+        }
+    }
+}
