ca1f1e6c05324c3b1f5231b69fd3a88e91271a6a
ArtemK123 <darkmode1012@gmail.com>
2020-09-20 12:11:04 +0300

    Added test for UserApiProxy.GetByTokenAsync and introduced UserApiProxyTestBase

---
+++ backend/dotnet/ticketflow/TicketFlow.IdentityService.Client.Test/Proxies/UserApiProxyTests/GetByTokenAsyncTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NSubstitute;
+using TicketFlow.Common.Exceptions;
+using TicketFlow.IdentityService.Client.Extensibility.Entities;
+using TicketFlow.IdentityService.Client.Extensibility.Models;
+using Xunit;
+
+namespace TicketFlow.IdentityService.Client.Test.Proxies.UserApiProxyTests
+{
+    public class GetByTokenAsyncTest : UserApiProxyTestBase
+    {
+        [Fact]
+        public async Task GetByTokenAsync_RequestUrl_ShouldSendRequestToRightEndpoint_Async()
+        {
+            await RunGetByTokenAsyncTestAsync(async _ =>
+            {
+                Uri expectedUri = new Uri($"{ServiceUrl}/users/getByToken");
+
+                await IdentityServiceMessageSenderMock.Received().SendAsync<UserSerializationModel>(
+                    Arg.Is<HttpRequestMessage>(message => Uri.Compare(expectedUri, message.RequestUri, UriComponents.AbsoluteUri, UriFormat.UriEscaped, StringComparison.Ordinal) == 0));
+            });
+        }
+
+        [Fact]
+        public async Task GetByTokenAsync_RequestUrl_ShouldGetServiceUrlFromProvider_Async()
+        {
+            await RunGetByTokenAsyncTestAsync(_ =>
+            {
+                IdentityServiceUrlProviderMock.Received().GetUrl();
+            });
+        }
+
+        [Fact]
+        public async Task GetByTokenAsync_RequestMethod_ShouldSendPostRequest_Async()
+        {
+            IdentityServiceMessageSenderMock.SendAsync<UserSerializationModel>(Arg.Any<HttpRequestMessage>()).ReturnsForAnyArgs(Task.FromResult(UserSerializationModel));
+            UserSerializerMock.Deserialize(UserSerializationModel).Returns(AuthorizedUserMock);
+
+            await UserApiProxy.GetByTokenAsync(Token);
+
+            await IdentityServiceMessageSenderMock.Received().SendAsync<UserSerializationModel>(
+                Arg.Is<HttpRequestMessage>(message => message.Method == HttpMethod.Post));
+        }
+
+        [Fact]
+        public async Task GetByTokenAsync_RequestBody_ShouldSendTokenAsRequestBody_Async()
+        {
+            await RunGetByTokenAsyncTestAsync(async _ =>
+            {
+                await IdentityServiceMessageSenderMock.Received().SendAsync<UserSerializationModel>(
+                    Arg.Is<HttpRequestMessage>(message => CheckHttpMessageBody(message, Token)));
+            });
+        }
+
+        [Fact]
+        public async Task GetByTokenAsync_ContentType_ShouldSetContentTypeAsTextPlain()
+        {
+            const string expectedContentType = "text/plain";
+
+            await RunGetByTokenAsyncTestAsync(async _ =>
+            {
+                await IdentityServiceMessageSenderMock.Received().SendAsync<UserSerializationModel>(
+                    Arg.Is<HttpRequestMessage>(
+                        message => message.Content.Headers.ContentType.MediaType.Equals(
+                                expectedContentType,
+                                StringComparison.Ordinal)));
+            });
+        }
+
+        [Fact]
+        public async Task GetByTokenAsync_SerializationModel_ShouldPassSerializationModelToProvider()
+        {
+            await RunGetByTokenAsyncTestAsync(_ =>
+            {
+                UserSerializerMock.Received().Deserialize(UserSerializationModel);
+            });
+        }
+
+        [Fact]
+        public async Task GetByTokenAsync_NonAuthorizedUser_ShouldThrowNotException()
+        {
+            IdentityServiceMessageSenderMock.SendAsync<UserSerializationModel>(default).ReturnsForAnyArgs(Task.FromResult(UserSerializationModel));
+            UserSerializerMock.Deserialize(default).ReturnsForAnyArgs(UserMock);
+
+            NotFoundException exception = await Assert.ThrowsAsync<NotFoundException>(() => UserApiProxy.GetByTokenAsync(Token));
+
+            string expectedExceptionMessage = $"User with token={Token} is not found";
+            Assert.Equal(expectedExceptionMessage, exception.Message);
+        }
+
+        private async Task RunGetByTokenAsyncTestAsync(Action<IAuthorizedUser> assertAction)
+        {
+            Task AssertActionAsync(IAuthorizedUser user) => Task.Run(() => assertAction(user));
+
+            await RunGetByTokenAsyncTestAsync(AssertActionAsync);
+        }
+
+        private async Task RunGetByTokenAsyncTestAsync(Func<IAuthorizedUser, Task> assertActionAsync)
+        {
+            IdentityServiceMessageSenderMock.SendAsync<UserSerializationModel>(default).ReturnsForAnyArgs(Task.FromResult(UserSerializationModel));
+            UserSerializerMock.Deserialize(default).ReturnsForAnyArgs(AuthorizedUserMock);
+
+            await UserApiProxy.GetByTokenAsync(Token);
+
+            await assertActionAsync(AuthorizedUserMock);
+        }
+
+        private bool CheckHttpMessageBody(HttpRequestMessage message, string expectedBody)
+        {
+            Task<string> getBodyTask = message.Content.ReadAsStringAsync();
+            getBodyTask.Wait();
+            string actualBody = getBodyTask.Result;
+
+            return expectedBody.Equals(actualBody, StringComparison.Ordinal);
+        }
+    }
+}
---
+++ backend/dotnet/ticketflow/TicketFlow.IdentityService.Client.Test/Proxies/UserApiProxyTests/UserApiProxyTestBase.cs
@@ -0,0 +1,50 @@
+﻿using NSubstitute;
+using TicketFlow.Common.Serializers;
+using TicketFlow.IdentityService.Client.Extensibility.Entities;
+using TicketFlow.IdentityService.Client.Extensibility.Models;
+using TicketFlow.IdentityService.Client.Extensibility.Serializers;
+using TicketFlow.IdentityService.Client.Providers;
+using TicketFlow.IdentityService.Client.Proxies;
+using TicketFlow.IdentityService.Client.Senders;
+
+namespace TicketFlow.IdentityService.Client.Test.Proxies.UserApiProxyTests
+{
+    public abstract class UserApiProxyTestBase
+    {
+        protected const string ServiceUrl = "http://localhost:9001";
+        protected const string Token = "jwt.jwt.jwt";
+
+        protected UserApiProxyTestBase()
+        {
+            UserSerializerMock = Substitute.For<IUserSerializer>();
+            IdentityServiceMessageSenderMock = Substitute.For<IIdentityServiceMessageSender>();
+            JsonSerializerMock = Substitute.For<IJsonSerializer>();
+            IdentityServiceUrlProviderMock = CreateIdentityServiceUrlProviderMock(ServiceUrl);
+
+            UserApiProxy = new UserApiProxy(UserSerializerMock, IdentityServiceMessageSenderMock, JsonSerializerMock, IdentityServiceUrlProviderMock);
+        }
+
+        protected UserSerializationModel UserSerializationModel { get; } = new UserSerializationModel();
+
+        protected IUser UserMock { get; } = Substitute.For<IUser>();
+
+        protected IAuthorizedUser AuthorizedUserMock { get; } = Substitute.For<IAuthorizedUser>();
+
+        protected IUserSerializer UserSerializerMock { get; }
+
+        protected IIdentityServiceMessageSender IdentityServiceMessageSenderMock { get; }
+
+        protected IJsonSerializer JsonSerializerMock { get; }
+
+        private protected IIdentityServiceUrlProvider IdentityServiceUrlProviderMock { get; }
+
+        private protected UserApiProxy UserApiProxy { get; }
+
+        private static IIdentityServiceUrlProvider CreateIdentityServiceUrlProviderMock(string url)
+        {
+            var mock = Substitute.For<IIdentityServiceUrlProvider>();
+            mock.GetUrl().Returns(url);
+            return mock;
+        }
+    }
+}
