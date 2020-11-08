90e5d7c7d8de09ddd3fb3e85777e27a3c405e506
ArtemK123 <darkmode1012@gmail.com>
2020-11-08 17:08:32 +0200

    Changed access modifiers in Startup files

--- backend/dotnet/ticketflow/TicketFlow.ApiGateway/Startup.cs
+++ backend/dotnet/ticketflow/TicketFlow.ApiGateway/Startup.cs
@@ -24,7 +24,7 @@ namespace TicketFlow.ApiGateway
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
