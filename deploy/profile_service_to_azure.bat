a07d481c071a229f8319dc6dface85d585460eaa
ArtemK123 <darkmode1012@gmail.com>
2020-12-06 11:41:48 +0200

    Deployed profile service to azure

---
+++ deploy/profile_service_to_azure.bat
@@ -0,0 +1,10 @@
+az container create ^
+	--resource-group TicketFlowResourceGroup ^
+	--name ticketflow-profile-service ^
+	--image darkmode1012/ticketflow_profile-service:1.0.1 ^
+	--cpu 1 ^
+	--memory 1 ^
+	--dns-name-label ticketflow-profile-service ^
+	--ports 9002 ^
+	--environment-variables ASPNETCORE_ENVIRONMENT=Azure ASPNETCORE_URLS=http://ticketflow-profile-service:9002
+	
