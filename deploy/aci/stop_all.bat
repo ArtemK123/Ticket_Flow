a0c5e47e594c2a13c464f50c73c54cbfffb631ff
ArtemK123 <darkmode1012@gmail.com>
2020-12-06 18:57:59 +0200

    Added scripts for starting and stoping all containers in ACI

---
+++ deploy/aci/stop_all.bat
@@ -0,0 +1,7 @@
+az container stop --name ticketflow-api-gateway --resource-group TicketFlowResourceGroup &^
+az container stop --name ticketflow-identity-service --resource-group TicketFlowResourceGroup &^
+az container stop --name ticketflow-profile-service --resource-group TicketFlowResourceGroup &^
+az container stop --name ticketflow-ticket-service --resource-group TicketFlowResourceGroup &^
+az container stop --name ticketflow-movie-service --resource-group TicketFlowResourceGroup &^
+az container stop --name ticketflow-consul --resource-group TicketFlowResourceGroup &^
+az container stop --name ticketflow-db --resource-group TicketFlowResourceGroup
