1626a3a187efd80080d9eb5ea02251a46df73bea
ArtemK123 <darkmode1012@gmail.com>
2020-12-08 20:02:52 +0200

    Added integration test project for ticket service

---
+++ backend/dotnet/ticketflow/TicketFlow.TicketService.IntegrationTest/TicketApiTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace TicketFlow.TicketService.IntegrationTest
+{
+    public class TicketApiTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> webApplicationFactory;
+
+        public TicketApiTest(WebApplicationFactory<Startup> webApplicationFactory)
+        {
+            this.webApplicationFactory = webApplicationFactory;
+        }
+
+        [Fact]
+        public async Task TestAsync()
+        {
+            HttpClient apiClient = webApplicationFactory.CreateClient();
+
+            HttpResponseMessage response = await apiClient.GetAsync("/tickets");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
