ad1e3e28c0fb28b73889e09f0623f16a7d40714d
ArtemK123 <darkmode1012@gmail.com>
2020-12-10 10:34:12 +0200

    Added support of consul on frontend side (backend side isn`t working cause of service registry duplication

--- frontend/ticketflow/src/services/backend_service/createConsulBasedBackendServiceAsync.js
+++ frontend/ticketflow/src/services/backend_service/createConsulBasedBackendServiceAsync.js
@@ -4,8 +4,10 @@ import createBackendRequestSender from "services/backend_service/createBackendRe
 
 const createConsulService = () => {
     const consulUrl = getEnvVariable("REACT_APP_CONSUL_URL");
-    const consulHostAndPort = consulUrl.split(":");
-    return createConsulServiceExternal({ host: consulHostAndPort[0], port: consulHostAndPort[1], promisify: true });
+    const consulUrlSplitted = consulUrl.split(":");
+    const consulHost = `${consulUrlSplitted[0]}:${consulUrlSplitted[1]}`;
+    const consulPort = consulUrlSplitted[2];
+    return createConsulServiceExternal({ host: consulHost, port: consulPort });
 };
 
 const consulService = createConsulService();
@@ -14,12 +16,18 @@ const createConsulBasedBackendServiceAsync = async (referrer_policy) => {
     return createBackendRequestSender(referrer_policy, async () => await getApiGatewayUrlAsync(consulService));
 };
 
-const getApiGatewayUrlAsync = async (consul) => {
-    const services = await consul.agent.service.list;
+const getApiGatewayUrlAsync = (consul) => {
+    return new Promise((resolve, reject) => {
+        consul.agent.service.list((err, services) => {
+            if (err || services === undefined) {
+                reject(err);
+            }
 
-    const apiGatewayServiceName = getEnvVariable("REACT_APP_API_GATEWAY_SERVICE_NAME");
-    const apiGatewayService = findApiGatewayService(services, apiGatewayServiceName);
-    return `${apiGatewayService.Address}:${apiGatewayService.Port}`;
+            const apiGatewayServiceName = getEnvVariable("REACT_APP_API_GATEWAY_SERVICE_NAME");
+            const apiGatewayService = findApiGatewayService(services, apiGatewayServiceName);
+            resolve(`${apiGatewayService.Address}:${apiGatewayService.Port}`);
+        });
+    });
 };
 
 const findApiGatewayService = (services, apiGatewayServiceName) => {
