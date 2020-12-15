136cbdc7b0664db807258b3792d445a46c700711
ArtemK123 <darkmode1012@gmail.com>
2020-12-15 12:20:45 +0200

    Covered Ticket service Api using proxy

--- backend/dotnet/ticketflow/TicketFlow.TicketService.IntegrationTest/ApiTests/TicketApiViaProxyTest.cs
+++ backend/dotnet/ticketflow/TicketFlow.TicketService.IntegrationTest/ApiTests/TicketApiViaProxyTest.cs
@@ -9,8 +9,10 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using NSubstitute;
+using TicketFlow.Common.Exceptions;
 using TicketFlow.TicketService.Client.Extensibility.DependencyInjection;
 using TicketFlow.TicketService.Client.Extensibility.Entities;
+using TicketFlow.TicketService.Client.Extensibility.Exceptions;
 using TicketFlow.TicketService.Client.Extensibility.Models;
 using TicketFlow.TicketService.Client.Extensibility.Proxies;
 using TicketFlow.TicketService.IntegrationTest.Fakes;
@@ -19,47 +21,119 @@ using Xunit;
 
 namespace TicketFlow.TicketService.IntegrationTest.ApiTests
 {
-    public class TicketApiViaProxyTest
+    public class TicketApiViaProxyTest : IDisposable
     {
+        private const string UserEmail = "test@gmail.com";
+        private const string AnotherUserEmail = "someboy@gmail.com";
+        private const int MovieId = 1;
+        private const int AnotherMovieId = 100;
+
+        private readonly WebApplicationFactory<Startup> webApplicationFactory;
+        private readonly ITicketApiProxy proxy;
+
+        public TicketApiViaProxyTest()
+        {
+            webApplicationFactory = SetupWebApplicationFactory();
+            proxy = webApplicationFactory.Services.GetService<ITicketApiProxy>();
+        }
+
+        public void Dispose()
+        {
+            webApplicationFactory?.Dispose();
+        }
+
         [Fact]
         public async Task Add_ThenGetByMovieId_ShouldReturnTicketsWithGivenMovieId()
         {
-            using WebApplicationFactory<Startup> webApplicationFactory = SetupWebApplicationFactory();
-            ITicketApiProxy proxy = webApplicationFactory.Services.GetService<ITicketApiProxy>();
-
-            const int movieId = 1;
-            const int anotherMovieId = 100;
-
             IReadOnlyCollection<TicketCreationModel> ticketCreationModelsToAdd = new[]
             {
-                new TicketCreationModel(movieId, 1, 1, 70),
-                new TicketCreationModel(movieId, 2, 2, 90),
-                new TicketCreationModel(movieId, 3, 5, 100),
-                new TicketCreationModel(anotherMovieId, 3, 5, 150),
+                new TicketCreationModel(MovieId, 1, 1, 70),
+                new TicketCreationModel(MovieId, 2, 2, 90),
+                new TicketCreationModel(MovieId, 3, 5, 100),
+                new TicketCreationModel(AnotherMovieId, 3, 5, 150),
             };
             await Task.WhenAll(ticketCreationModelsToAdd.Select(model => proxy.AddAsync(model)));
 
-            IReadOnlyCollection<ITicket> tickets = await proxy.GetByMovieIdAsync(movieId);
+            IReadOnlyCollection<ITicket> tickets = await proxy.GetByMovieIdAsync(MovieId);
 
-            IReadOnlyCollection<TicketCreationModel> ticketCreationApiModelsWithGivenMovieId = ticketCreationModelsToAdd.Where(model => model.MovieId == movieId).ToArray();
+            IReadOnlyCollection<TicketCreationModel> ticketCreationApiModelsWithGivenMovieId = ticketCreationModelsToAdd.Where(model => model.MovieId == MovieId).ToArray();
             Assert.True(ticketCreationApiModelsWithGivenMovieId.All(ticketCreationModel => tickets.Any(ticket => SameTicket(ticketCreationModel, ticket))));
         }
 
         [Fact]
-        public async Task Order_ShouldUpdateTicketWithUserEmail() {
-            using WebApplicationFactory<Startup> webApplicationFactory = SetupWebApplicationFactory();
-            ITicketApiProxy proxy = webApplicationFactory.Services.GetService<ITicketApiProxy>();
-
+        public async Task Order_Valid_ShouldUpdateTicketWithUserEmail()
+        {
             TicketCreationModel ticketCreationModel = new TicketCreationModel(1, 1, 1, 70);
             await proxy.AddAsync(ticketCreationModel);
             ITicket ticket = (await proxy.GetByMovieIdAsync(ticketCreationModel.MovieId)).First();
 
-            const string userEmail = "test@gmail.com";
-            await proxy.OrderAsync(new OrderModel(ticket.Id, userEmail));
+            await proxy.OrderAsync(new OrderModel(ticket.Id, UserEmail));
 
-            // IOrderedTicket orderedTicket = (await proxy.GetByUserEmailAsync(userEmail)).First();
-            //
-            // Assert.True(orderedTicket.)
+            IOrderedTicket orderedTicket = (await proxy.GetByUserEmailAsync(UserEmail)).First();
+
+            Assert.True(orderedTicket.BuyerEmail == UserEmail && SameTicket(ticketCreationModel, orderedTicket));
+        }
+
+        [Fact]
+        public async Task Order_AlreadyOrderedTicket_ShouldThrowTicketAlreadyOrderedException()
+        {
+            TicketCreationModel ticketCreationModel = new TicketCreationModel(1, 1, 1, 70);
+            await proxy.AddAsync(ticketCreationModel);
+            ITicket ticket = (await proxy.GetByMovieIdAsync(ticketCreationModel.MovieId)).First();
+
+            await proxy.OrderAsync(new OrderModel(ticket.Id, UserEmail));
+
+            TicketAlreadyOrderedException exception = await Assert.ThrowsAsync<TicketAlreadyOrderedException>(
+                async () => await proxy.OrderAsync(new OrderModel(ticket.Id, UserEmail)));
+
+            Assert.Equal($"Ticket with id={ticket.Id} is already ordered", exception.Message);
+        }
+
+        [Fact]
+        public async Task Order_WrongTicketId_ShouldThrowNotFoundException()
+        {
+            const int notFoundTicketId = 1;
+            NotFoundException exception = await Assert.ThrowsAsync<NotFoundException>(
+                async () => await proxy.OrderAsync(new OrderModel(notFoundTicketId, UserEmail)));
+
+            Assert.Equal($"Ticket with id={notFoundTicketId} is not found", exception.Message);
+        }
+
+        [Fact]
+        public async Task Add_ThenOrderSome_ThenGetByUserEmail_ShouldReturnAllTicketsWithGivenUserEmail()
+        {
+            IReadOnlyCollection<TicketCreationModel> notOrderedTickets = new[]
+            {
+                CreateDefaultTicketCreationModel(),
+                CreateDefaultTicketCreationModel()
+            };
+
+            IReadOnlyCollection<(TicketCreationModel, string)> ticketToOrderWithUserEmailPairs = new[]
+            {
+                (CreateDefaultTicketCreationModel(), UserEmail),
+                (CreateDefaultTicketCreationModel(), UserEmail),
+                (CreateDefaultTicketCreationModel(), AnotherUserEmail)
+            };
+
+            foreach (TicketCreationModel ticketCreationModel in notOrderedTickets)
+            {
+                await proxy.AddAsync(ticketCreationModel);
+            }
+
+            foreach ((TicketCreationModel ticketCreationModel, string userEmail) in ticketToOrderWithUserEmailPairs)
+            {
+                var ticketId = await proxy.AddAsync(ticketCreationModel);
+                await proxy.OrderAsync(new OrderModel(ticketId, userEmail));
+            }
+
+            IReadOnlyCollection<TicketCreationModel> expectedTicketModels = ticketToOrderWithUserEmailPairs
+                .Where(ticketWithEmail => ticketWithEmail.Item2 == UserEmail)
+                .Select(ticketWithEmail => ticketWithEmail.Item1)
+                .ToArray();
+
+            IReadOnlyCollection<IOrderedTicket> actual = await proxy.GetByUserEmailAsync(UserEmail);
+
+            Assert.True(expectedTicketModels.Count == actual.Count && expectedTicketModels.All(ticketCreationModel => actual.Any(ticket => SameTicket(ticketCreationModel, ticket))));
         }
 
         private static bool SameTicket(TicketCreationModel creationModel, ITicket ticket)
@@ -68,11 +142,13 @@ namespace TicketFlow.TicketService.IntegrationTest.ApiTests
                && creationModel.Seat == ticket.Seat
                && creationModel.Price == ticket.Price;
 
-        private WebApplicationFactory<Startup> SetupWebApplicationFactory()
+        private static TicketCreationModel CreateDefaultTicketCreationModel() => new TicketCreationModel(default, default, default, default);
+
+        private static WebApplicationFactory<Startup> SetupWebApplicationFactory()
         {
             HttpClient client = default;
 
-            WebApplicationFactory<Startup> webApplicationFactory = new WebApplicationFactory<Startup>()
+            WebApplicationFactory<Startup> localWebApplicationFactory = new WebApplicationFactory<Startup>()
                 .WithWebHostBuilder(builder =>
                 {
                     builder.UseEnvironment("Test");
@@ -92,9 +168,9 @@ namespace TicketFlow.TicketService.IntegrationTest.ApiTests
                     });
                 });
 
-            client = webApplicationFactory.CreateClient();
+            client = localWebApplicationFactory.CreateClient();
 
-            return webApplicationFactory;
+            return localWebApplicationFactory;
         }
     }
 }
