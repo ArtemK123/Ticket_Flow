d9146c632e20fdeb8db7135e09e73f954f77eaf6
ArtemK123 <darkmode1012@gmail.com>
2020-12-16 11:53:50 +0200

    Expanded exception handling in profile service response validator

--- backend/dotnet/ticketflow/TicketFlow.ProfileService.Client/Validators/ProfileServiceResponseValidator.cs
+++ backend/dotnet/ticketflow/TicketFlow.ProfileService.Client/Validators/ProfileServiceResponseValidator.cs
@@ -18,38 +18,19 @@ namespace TicketFlow.ProfileService.Client.Validators
 
         public async Task ValidateAsync(HttpResponseMessage httpResponse)
         {
-            if (httpResponse.StatusCode == HttpStatusCode.NotFound && exceptionHeaderHandler.IsExceptionInHeader<ProfileNotFoundByIdException>(httpResponse.Headers))
+            switch (httpResponse.StatusCode)
             {
-                throw new ProfileNotFoundByIdException(await httpResponse.Content.ReadAsStringAsync());
+                case HttpStatusCode.NotFound when exceptionHeaderHandler.IsExceptionInHeader<ProfileNotFoundByIdException>(httpResponse.Headers):
+                    throw new ProfileNotFoundByIdException(await httpResponse.Content.ReadAsStringAsync());
+                case HttpStatusCode.NotFound when exceptionHeaderHandler.IsExceptionInHeader<ProfileNotFoundByUserEmailException>(httpResponse.Headers):
+                    throw new ProfileNotFoundByUserEmailException(await httpResponse.Content.ReadAsStringAsync());
+                case HttpStatusCode.BadRequest when exceptionHeaderHandler.IsExceptionInHeader<NotUniqueUserProfileException>(httpResponse.Headers):
+                    throw new NotUniqueUserProfileException(await httpResponse.Content.ReadAsStringAsync());
+                case HttpStatusCode.BadRequest:
+                    throw new InvalidRequestModelException(await httpResponse.Content.ReadAsStringAsync());
+                case HttpStatusCode.InternalServerError:
+                    throw new InternalServiceException("Internal error in Profile Service");
             }
-
-            if (httpResponse.StatusCode == HttpStatusCode.NotFound && exceptionHeaderHandler.IsExceptionInHeader<ProfileNotFoundByUserEmailException>(httpResponse.Headers))
-            {
-                throw new ProfileNotFoundByUserEmailException(await httpResponse.Content.ReadAsStringAsync());
-            }
-
-            if (httpResponse.StatusCode == HttpStatusCode.BadRequest && exceptionHeaderHandler.IsExceptionInHeader<NotUniqueUserProfileException>(httpResponse.Headers))
-            {
-                throw new NotUniqueUserProfileException(await httpResponse.Content.ReadAsStringAsync());
-            }
-
-            if (httpResponse.StatusCode == HttpStatusCode.BadRequest)
-            {
-                throw new InternalServiceException("Internal error in Profile Service");
-            }
-
-            if (httpResponse.StatusCode == HttpStatusCode.InternalServerError)
-            {
-                throw new InternalServiceException("Internal error in Profile Service");
-            }
-
-
-            // switch (httpResponse.StatusCode)
-            // {
-            //     case HttpStatusCode.NotFound: throw new NotFoundException(await httpResponse.Content.ReadAsStringAsync());
-            //     case HttpStatusCode.BadRequest: throw new NotUniqueEntityException(await httpResponse.Content.ReadAsStringAsync());
-            //     case HttpStatusCode.InternalServerError: throw new InternalServiceException("Internal error in Profile Service");
-            // }
         }
     }
 }
